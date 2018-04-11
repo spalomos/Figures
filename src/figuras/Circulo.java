d04e34489308e0f15b92673bae366bc36a68a690
cfgs <cfgs@pc-info-host>
2018-04-11 20:40:54 +0200

    area and perimetre

--- src/figuras/Circulo.java
+++ src/figuras/Circulo.java
@@ -16,7 +16,14 @@ public class Circulo {
 	public void setRadio(Integer radio) {
 		this.radio = radio;
 	}
-	
-	
+
+	public double area(Integer radi) {
+		return radi * radi * Math.PI;
+	}
+
+	public double perimetre(Integer radi) {
+		return 2 * radi * Math.PI;
+	}
+
 
 }
